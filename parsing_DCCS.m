26283dfe99d249adb8ca9670c4a5f923072f4099
Jesse Miles <jessemile@gmail.com>
2024-05-07 11:00:00 -0700

    Update parsing_DCCS.m

--- parsing_DCCS.m
+++ parsing_DCCS.m
@@ -1,10 +1,24 @@
 [fname, path] = uigetfile("*.csv", "Pick DCCS .csv file");
 cd(path)
 
-trialtab = readtable([path fname]);
+trialtab = readtable([path fname]); 
+varnames = trialtab.Properties.VariableNames';
 keepcols = ["Trial";"cResp";"TestStimulus_RESP";"TestStimulus_ACC";...
             "Stimulus";"MixedTrialList"];
-varnames = trialtab.Properties.VariableNames';
+if any(contains(varnames,"AudioCue"))
+    % rename the old cue variable, which is not the target cue
+    trialtab = renamevars(trialtab,"Cue","XcueX"); 
+    trialtab = renamevars(trialtab,"AudioCue","Cue");
+    keepcols = [keepcols;"Cue"];
+elseif any(contains(varnames,"Cue"))
+    % WARNING - v2 files also contain "Cue", but it's a different variable!
+    keepcols = [keepcols;"Cue"];
+else
+    warning("Did not find a 'Cue' column with expected format")
+end
+
+
+keeptab = trialtab(:,keepcols);
 
 beh_data = convert_behav_data(trialtab);
 
@@ -25,24 +39,29 @@ for iter = 1:niter
     end
     smat(:,:,iter) = cur_mat;
 end
-cur_table = array2table(mean(smat,3),"VariableNames",strategies);
+all_data = array2table(mean(smat,3),"VariableNames",strategies);
+flex = table2array(sum(abs(diff(all_data(:,strategies))),2));
 
-if height(cur_table)>height(beh_data) && (exist('npad','var')==1 && npad>0)
-    cur_table = cur_table(npad+1:end,:);
+if height(all_data)>height(beh_data) && (exist('npad','var')==1 && npad>0)
+    all_data = all_data(npad+1:end,:);
 end
 
-all_data = [beh_data, cur_table];
 all_data = renamevars(all_data,"TrialIndex","trial");
 all_data.trial = (1:height(all_data))';
 % plot using a "try/catch" which will plot a nice figure if all 4 blocks
 % are completed and a more basic figure if they aren't. 
-figure, plot(all_data,"trial",strategies)
+figure, plot(all_data,"trial",strategies,"LineWidth",2)
 xlim([min(all_data.trial) max(all_data.trial)]), ylim([0 1])
 ylabel("Strategy Likelihood"); xlabel("Trial")
+yticks(0.1:0.2:0.9); xticks(5:10:95)
 cmap = colororder;
 for s = 1:numel(strategies)
     y = table2array(all_data(end,strategies(s)));
     text(101,y,replace(strategies(s),"pick_"," "),"Color",cmap(s,:))
 end
+
+figure
+plot(all_data.trial(2:end), flex, Color="k", LineWidth=2)
+ylim([0 0.15]);
 
 % trial_table = all_data;
